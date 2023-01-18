a8d1121a7afb221c0d68ecfe3338223de84e9eeb
Junhyuck Ko <mrbartrns@naver.com>
2023-01-18 12:56:00 +0900

    chore: 개발 환경 설정

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+/** @type {import('jest').Config} */
+
 module.exports = {
+  verbose: true,
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
     '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
+  transform: {
+    '\\.(j|t)sx?$': 'babel-jest',
+  },
 };
